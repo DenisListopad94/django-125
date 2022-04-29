2caef05cf014faca424506fdb2f2255407cd8030
Denis <kuku.ru>
2022-04-30 01:10:26 +0300

    manytomany onetoone

--- group_125/cars/admin.py
+++ group_125/cars/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from cars.models import Car,Company
+from cars.models import *
 # Register your models here.
 
 
@@ -9,7 +9,15 @@ class CarsAdmin(admin.ModelAdmin):
     search_fields = ('title','costs')
     list_editable = ('costs','create')
     list_filter = ('title','costs','create')
+    filter_horizontal = ('provider',)
+    ordering = ('title','id')
     # list_per_page = 2 # пагинация
+
+class DirectorAdmin(admin.ModelAdmin):
+    list_display = ('id','name','company')
+    list_display_links = ('id','name')
 
 admin.site.register(Car,CarsAdmin)
 admin.site.register(Company)
+admin.site.register(Provider)
+admin.site.register(Director,DirectorAdmin)
--- group_125/cars/models.py
+++ group_125/cars/models.py
@@ -1,18 +1,38 @@
 from django.db import models
 
+
 class Company(models.Model):
-    compony = models.TextField(verbose_name='компания',null = True)
-    year_budjet = models.IntegerField(verbose_name='годовой доход',null = True)
+    compony = models.TextField(verbose_name='компания', null=True)
+    year_budjet = models.IntegerField(verbose_name='годовой доход', null=True)
+    director = models.OneToOneField('Director', on_delete=models.CASCADE, null=True)
+
     def __str__(self):
         return self.compony
 
+
+class Director(models.Model):
+    name = models.CharField(max_length=20, verbose_name='Имя')
+    surname = models.CharField(max_length=20, verbose_name='Фамилия')
+
+    def __str__(self):
+        return self.name
+
+
+class Provider(models.Model):
+    provider = models.CharField(max_length=40, verbose_name='компания поставщик')
+    type_detail = models.TextField(verbose_name='тип деталей')
+
+    def __str__(self):
+        return self.provider
+
+
 class Car(models.Model):
-    title = models.CharField(max_length=40,verbose_name='машины') # название машинок
+    title = models.CharField(max_length=40, verbose_name='машины')  # название машинок
     content = models.TextField(verbose_name='описание')
     costs = models.IntegerField(verbose_name='стоимость')
-    create = models.IntegerField(default=2010,verbose_name='дата выпуска')
-    company = models.ForeignKey('Company',on_delete=models.CASCADE,null=True)
-
+    create = models.IntegerField(default=2010, verbose_name='дата выпуска')
+    company = models.ForeignKey('Company', on_delete=models.CASCADE, null=True)
+    provider = models.ManyToManyField(Provider)
 
     class Meta():
         verbose_name = 'Мaшины'
@@ -20,5 +40,3 @@ class Car(models.Model):
 
     def __str__(self):
         return self.title
-
-
